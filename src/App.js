7d509b9e6f7eebafa005d7670644cec099a4fc19
Chaitanya360 <chaitanyajadhav360@gmail.com>
2022-06-18 00:04:10 +0530

    started navigator

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import './App.css';
-import FullScreenButton from './Component/Elements/FullScreenButton';
-import Globe from './Component/Elements/Globe';
-import HideOverlay from './Component/Elements/HideOverlay';
-
+import "./App.css";
+import { AppStyle } from "./App.style";
+import FullScreenButton from "./Component/Elements/FullScreenButton";
+import Globe from "./Component/Elements/Globe";
+import HideOverlay from "./Component/Elements/HideOverlay";
+import Navigator from "./Component/Molecules/Navigator";
 
 function App() {
-  return <>
-    {/* <FullScreenButton /> */}
-    <HideOverlay />
-    <Globe />
-  </>
+  return (
+    <AppStyle>
+      <Navigator />
+    </AppStyle>
+  );
 }
 
 export default App;
